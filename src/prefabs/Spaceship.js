269b47eabf3584e9451745b416ce394145e40214
Kayla <kthuyt08@gmail.com>
2025-01-14 12:24:01 -0800

    add sound fx

---
+++ src/prefabs/Spaceship.js
@@ -0,0 +1,26 @@
+// Spaceship prefab
+class Spaceship extends Phaser.GameObjects.Sprite {
+    constructor(scene, x, y, texture, frame, pointValue) {
+      super(scene, x, y, texture, frame)
+  
+      // add object to existing scene
+      scene.add.existing(this)
+      this.points = pointValue
+      this.moveSpeed = 3
+    }
+
+    update() {
+        // move spaceship left
+        this.x -= this.moveSpeed
+
+        // wrap from left to right edge
+        if(this.x <= 0 - this.width) {
+            this.x = game.config.width
+        }
+    }
+
+    // reset position
+    reset() {
+        this.x = game.config.width
+    }
+}
