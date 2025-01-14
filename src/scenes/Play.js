51b12eae0d15fe8f7258322f1b1ae8a828239627
Kayla <kthuyt08@gmail.com>
2025-01-14 13:48:22 -0800

    update game over and menu

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -50,9 +50,9 @@ class Play extends Phaser.Scene {
         
         // 60-second play clock
         scoreConfig.fixedWidth = 0
-        this.clock = this.time.delayedCall(60000, () => {
+        this.clock = this.time.delayedCall(game.settings.gameTimer, () => {
             this.add.text(game.config.width/2, game.config.height/2, 'GAME OVER', scoreConfig).setOrigin(0.5)
-            this.add.text(game.config.width/2, game.config.height/2 + 64, 'Press (R) to Restart', scoreConfig).setOrigin(0.5)
+            this.add.text(game.config.width/2, game.config.height/2 + 64, 'Press (R) to Restart or <- for Menu', scoreConfig).setOrigin(0.5)
             this,gameOver = true
         }, null, this)
     }
@@ -63,6 +63,11 @@ class Play extends Phaser.Scene {
             this.scene.restart()
         }
         this.starfield.tilePositionX -= 4
+
+        // check key input for menu
+        if (this.gameOver && Phaser.Input.Keyboard.JustDown(keyLEFT)) {
+            this.scene.start("menuScene")
+          }
 
         if(!this.gameOver) {    
             // update rocket sprite           
